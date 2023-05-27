9940ee9a270acfbdf647368fbc1effda5ef4f18d
Citizen1 <daltonhrrll@gmail.com>
2023-05-27 05:00:31 -0500

    Update wordlister.py

--- wordlister.py
+++ wordlister.py
@@ -2,32 +2,31 @@
 
 import sys
 import multiprocessing as mp
-from tqdm import tqdm
 import re
 
-def combine_lines(line1, file2, output, remove_symbols, remove_numbers, make_lowercase):
-    with open(file2) as f2, open(output, 'a') as out:
-        for line2 in f2:
-            combined = line1.strip() + line2.strip()
+def modify_line(line, output, remove_symbols, remove_numbers, make_lowercase):
+    modified = line.strip()
 
-            if remove_symbols:
-                # Remove all non-alphanumeric characters from the string
-                combined = re.sub('[^0-9a-zA-Z]+', '', combined)
+    if remove_symbols:
+        # Remove all non-alphanumeric characters from the string
+        modified = re.sub('[^0-9a-zA-Z]+', '', modified)
 
-            if remove_numbers:
-                # Remove all digits from the string
-                combined = re.sub('[0-9]+', '', combined)
+    if remove_numbers:
+        # Remove all digits from the string
+        modified = re.sub('[0-9]+', '', modified)
 
-            if make_lowercase:
-                # Convert the string to lowercase
-                combined = combined.lower()
+    if make_lowercase:
+        # Convert the string to lowercase
+        modified = modified.lower()
 
-            out.write(combined + '\n')
+    with open(output, 'a') as out:
+        out.write(modified + '\n')
 
 if __name__ == '__main__':
-    if len(sys.argv) < 4 or '-h' in sys.argv:
-        print("Usage: combine_wordlists.py [-ns] [-nn] [-l] file1.txt file2.txt output.txt\n")
+    if len(sys.argv) < 3 or '-h' in sys.argv:
+        print("Usage: combine_wordlists.py [-c] [-ns] [-nn] [-l] input.txt output.txt\n")
         print("Options:")
+        print("-c       Clean the input file according to the provided options and output the modified file")
         print("-ns      Remove all symbols from the output")
         print("-nn      Remove all numbers from the output")
         print("-l       Make everything in the output lowercase")
@@ -36,40 +35,39 @@ if __name__ == '__main__':
 
     # Parse command-line arguments
     args = sys.argv[1:]
+    clean_mode = '-c' in args
     remove_symbols = '-ns' in args
     remove_numbers = '-nn' in args
     make_lowercase = '-l' in args
-    file1, file2, output = [arg for arg in args if arg not in ('-ns', '-nn', '-l', '-h')][0:3]
+    input_file, output_file = [arg for arg in args if arg not in ('-c', '-ns', '-nn', '-l', '-h')][0:2]
 
-    # Count the number of lines in each file
-    with open(file1) as f1:
-        num_lines1 = sum(1 for _ in f1)
-    with open(file2) as f2:
-        num_lines2 = sum(1 for _ in f2)
+    if clean_mode:
+        # Count the number of lines in the file
+        with open(input_file) as f:
+            num_lines = sum(1 for _ in f)
 
-    # Determine the total number of combinations
-    total_combinations = num_lines1 * num_lines2
+        # Get the maximum number of threads available on the system
+        max_threads = mp.cpu_count()
 
-    # Get the maximum number of threads available on the system
-    max_threads = mp.cpu_count()
+        # Prompt the user for the number of threads to use
+        num_threads = int(input(f"How many threads do you want to use (up to {max_threads})? "))
 
-    # Prompt the user for the number of threads to use
-    num_threads = int(input(f"How many threads do you want to use (up to {max_threads})? "))
+        # Check that the number of threads is not greater than the maximum
+        if num_threads > max_threads:
+            print(f"Error: the maximum number of threads available on this system is {max_threads}")
+            sys.exit(1)
 
-    # Check that the number of threads is not greater than the maximum
-    if num_threads > max_threads:
-        print(f"Error: the maximum number of threads available on this system is {max_threads}")
-        sys.exit(1)
+        print(f"Cleaning {input_file} into {output_file} using {num_threads} threads...")
 
-    print(f"Combining {file1} and {file2} into {output} using {num_threads} threads...")
+        # Initialize the process pool and start the cleaning process
+        with mp.Pool(num_threads) as pool:
+            for line in open(input_file):
+                pool.apply_async(modify_line, args=(line, output_file, remove_symbols, remove_numbers, make_lowercase))
 
-    # Initialize the process pool and start the combination process
-    with mp.Pool(num_threads) as pool, tqdm(total=total_combinations) as pbar:
-        for line1 in open(file1):
-            pool.apply_async(combine_lines, args=(line1, file2, output, remove_symbols, remove_numbers, make_lowercase), callback=lambda _: pbar.update(num_lines2))
+            # Wait for all processes to finish before exiting
+            pool.close()
+            pool.join()
 
-        # Wait for all processes to finish before exiting
-        pool.close()
-        pool.join()
-
-    print(f"Successfully combined {file1} and {file2} into {output}")
+        print(f"Successfully cleaned {input_file} into {output_file}")
+    else:
+        print("No cleaning option selected. Please provide '-c' for cleaning.")
